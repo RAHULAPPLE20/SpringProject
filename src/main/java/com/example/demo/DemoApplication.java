4e31d03ee8d19aed6832928be3489644f4867424
RAHULAPPLE20 <karanjavkar@gmail.com>
2023-08-05 21:05:52 +0530

    Add files via upload

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,13 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-	@GetMapping
+	@GetMapping("/")
 	public String getMessage()
 	{
 		return "Hi Open shift";
@@ -20,6 +22,7 @@ public class DemoApplication {
 	{
 		return "Your Name is"+name;
 	}
+	
 	
 	
 	public static void main(String[] args) {
